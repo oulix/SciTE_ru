f92bcf3098d3d7b5df0bacd3f93713cd6fbcf863
jou <oulix@hotmail.com>
2014-05-23 10:01:57 +0800

    show search and replace with dialog but not panel on the bottom; translate FindText.lua to English

--- tools/FindText.lua
+++ tools/FindText.lua
@@ -1,19 +1,19 @@
 --[==[--------------------------------------------------
-FindText v8.1.0
-Авторы: mozers™, mimir, Алексей, codewarlock1101, VladVRO, Tymur Gubayev
+FindText v8.1.0 
+Authors: mozers ™, mimir, Alexei, codewarlock1101, VladVRO, Tymur Gubayev 
 
-* Если текст выделен - ищется выделенная подстрока
-* Если текст не выделен - ищется текущее слово
-* Поиск возможен как в окне редактирования, так и в окне консоли
-* Строки, содержащие результаты поиска, выводятся в консоль
-* Перемещение по вхождениям - F3 (вперед), Shift+F3 (назад)
-* Каждый новый поиск оставляет маркеры своего цвета
-* Очистка от маркеров поиска - Ctrl+Alt+C
+* If text is highlighted - searched substring 
+* If no text is selected - the current word is searched 
+* The search is available in the editing window, and in the console window 
+* Lines containing the search results are displayed in the console 
+* Navigating the occurrences - F3 (forward), Shift + F3 (back) 
+* Every new search leaves markers of the same color 
+* Cleaning of search markers - Ctrl + Alt + C 
 
-Внимание:
-В скрипте используются функции из COMMON.lua (EditorMarkText, EditorClearMarks)
+note: 
+The script uses a function of COMMON.lua (EditorMarkText, EditorClearMarks)
 -----------------------------------------------
-Для подключения добавьте в свой файл .properties следующие строки:
+To connect to your add. Properties following lines:
 	command.name.130.*=Find String/Word
 	command.130.*=dostring loadfile([[$(SciteDefaultHome)\tools\FindText.lua]])($(findtext.first.mark))
 	command.mode.130.*=subsystem:lua,savebefore:no
@@ -24,7 +24,7 @@ FindText v8.1.0
 	command.mode.131.*=subsystem:lua,savebefore:no
 	command.shortcut.131.*=Ctrl+Alt+C
 
-Дополнительно необходимо задать в файле настроек стили используемых маркеров и номер первого из используемых стилей:
+Additionally, you must specify in the configuration file used by the styles of markers and the number of the first of the styles:
 	findtext.first.mark=27
 	indic.style.27=#CC00FF
 	indic.style.28=#0000FF
@@ -32,14 +32,14 @@ FindText v8.1.0
 	indic.style.30=#CCCC00
 	indic.style.31=#336600
 
-Дополнительно можно задать дополнительные параметры поиска:
-	# Поиск с учетом регистра
+Additionally, you can specify additional search options:
+	# Sensitive search
 	findtext.matchcase=1
-	# Отмечать букмарками найденные строки
+	# Celebrate bukmarki found rows
 	findtext.bookmarks=1
-	# Выводить все найденные строки в консоль
+	# Print all lines found in the console
 	findtext.output=1
-	# Показывать подсказку по горячим клавишам
+	# Show help on hot keys
 	findtext.tutorial=1
 --]==]----------------------------------------------------
 
@@ -113,7 +113,7 @@ if sText ~= '' then
 	if current_mark_number > 31 then current_mark_number = firstNum end
 	-- scite.SendEditor(SCI_SETINDICATORCURRENT, current_mark_number)
 	props['command.80.*']=[=[dostring loadfile([[$(SciteDefaultHome)\tools\FindText.lua]])]=]..'('..current_mark_number..')'
-		-- обеспечиваем возможность перехода по вхождениям с помощью F3 (Shift+F3)
+		-- provide an opportunity for transition occurrences using F3 (Shift + F3)
 		if flag0 == SCFIND_WHOLEWORD then
 			editor:GotoPos(editor:WordStartPosition(editor.CurrentPos))
 		else
@@ -128,4 +128,4 @@ else
 	print('> '..L'Or put cursor on the word for search. (search for word)' )
 	print('> '..L'You can also select text in console.' )
 end
---~ editor:CharRight() editor:CharLeft() --Снимает выделение с исходного текста
+--~ editor:CharRight() editor:CharLeft() --Deselects the original text
